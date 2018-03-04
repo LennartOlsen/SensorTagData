1fa00f63a2ae0660f1c880dea1f6881428c1f0cf
Lennart Olsen <lo@cardlay.com>
2018-03-04 13:39:45 +0100

    Initial Commit

---
+++ get.sh
@@ -0,0 +1,8 @@
+scp -r lo@dvalinn:~/jupyter/activityClassifier.model ./
+scp lo@dvalinn:~/jupyter/activityClassifier.mlmodel ./
+
+scp -r lo@dvalinn:~/jupyter/timeFramedActivityClassifier.model ./
+scp lo@dvalinn:~/jupyter/timeFramedActivityClassifier.mlmodel ./
+
+scp lo@dvalinn:"~/jupyter/Time\ Framed\ Training\ Data.ipynb" ./
+scp lo@dvalinn:"~/jupyter/Training\ Data.ipynb" ./
---
+++ push.sh
@@ -0,0 +1,4 @@
+scp ./trainingData.csv lo@dvalinn:~/jupyter/
+scp ./validationData.csv lo@dvalinn:~/jupyter/
+scp ./timeFramedTrainingData.csv lo@dvalinn:~/jupyter/
+scp ./timeFramedValidationData.csv lo@dvalinn:~/jupyter/
