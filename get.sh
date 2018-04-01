d490b8799d0c44fa56e47d74e38c6f6c32279ffd
Lennart Olsen <lo@cardlay.com>
2018-04-01 12:31:27 +0200

    Updated to confirm to new models

--- get.sh
+++ get.sh
@@ -1,8 +1,4 @@
 scp -r lo@dvalinn:~/jupyter/activityClassifier.model ./
 scp lo@dvalinn:~/jupyter/activityClassifier.mlmodel ./
 
-scp -r lo@dvalinn:~/jupyter/timeFramedActivityClassifier.model ./
-scp lo@dvalinn:~/jupyter/timeFramedActivityClassifier.mlmodel ./
-
-scp lo@dvalinn:"~/jupyter/Time\ Framed\ Training\ Data.ipynb" ./
 scp lo@dvalinn:"~/jupyter/Training\ Data.ipynb" ./
--- push.sh
+++ push.sh
@@ -1,4 +1,2 @@
 scp ./trainingData.csv lo@dvalinn:~/jupyter/
 scp ./validationData.csv lo@dvalinn:~/jupyter/
-scp ./timeFramedTrainingData.csv lo@dvalinn:~/jupyter/
-scp ./timeFramedValidationData.csv lo@dvalinn:~/jupyter/
