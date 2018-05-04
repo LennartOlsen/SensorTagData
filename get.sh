7faeeeb70ff9e4b82c724b9cdc2304d86b7851a8
Lennart Olsen <lo@cardlay.com>
2018-05-04 14:11:48 +0200

    Updated ipython notebook

--- get.sh
+++ get.sh
@@ -1,4 +1,4 @@
 scp -r lo@dvalinn:~/jupyter/activityClassifier.model ./
 scp lo@dvalinn:~/jupyter/activityClassifier.mlmodel ./
 
-scp lo@dvalinn:"~/jupyter/Training\ Data.ipynb" ./
+scp lo@dvalinn:"~/jupyter/Time\ Framed\ Training\ Data.ipynb" ./
